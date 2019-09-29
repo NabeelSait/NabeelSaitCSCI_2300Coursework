097e5d246cfa6b6371ef0f4f51866f7bcd20c434
saitn saitn <saitn@hopper.slu.edu>
2019-09-29 13:08:41 -0500

    test

--- lab1/Driver.java
+++ lab1/Driver.java
@@ -6,7 +6,7 @@ public class Driver
     Point2D c = new Point2D(2,3);
     Circle2D cir2 = new Circle2D(3, c);
 
-    System.out.println("Cirlce 1: ");
+    System.out.println("Cirlce 0: ");
     System.out.println(cir1);
     System.out.println("\nCircle 2: ");
     System.out.println(cir2);
