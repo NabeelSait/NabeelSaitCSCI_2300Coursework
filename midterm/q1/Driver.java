fa7b08bb26ce1acfb1f9ecaa9b8e0ff935eb5eba
Nabeel Sait <nabeel.sait@slu.edu>
2019-10-16 12:45:36 -0500

    Question 1

--- midterm/q1/Driver.java
+++ midterm/q1/Driver.java
@@ -2,27 +2,43 @@
  * Fix compiler and run-timme errors of this code.
  */
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class WriteToFileDriver
+public class Driver
 {
    public static void write(ArrayList<Integer> data, String fileName)
    {
        FileWriter writer = null;
-       writer = new FileWriter(fileName);
+       try
+       {
+         writer = new FileWriter(fileName);
+       }
+       catch(IOException e)
+       {
+          System.out.println("IOException Error");
+       }
+
        for (int i = 0; i < data.size(); i++)
        {
-          writer.write(data.get(i));
+          try
+          {
+             writer.write(data.get(i));
+          }
+          catch(IOException e)
+          {
+             System.out.println("IOException Error");
+          }
        }
 
    }
    public static void main(String []args)
    {
-      ArrayList<Integer> myNumbers = null;
+      ArrayList<Integer> myNumbers = new ArrayList<Integer>();
       for(int i = 0; i < 10; i++)
       {
           myNumbers.add(i);
       }
-       
       write(myNumbers, "./results.txt");
    }
 }
