70b50bf4a3536c8250bf45d280bb8fa3da2f6fa8
Nabeel Sait <nabeel.sait@slu.edu>
2019-12-08 19:46:43 -0600

    Part 1 Complete

--- finalExam/model/dataLoaders/InventoryFromCSV.java
+++ finalExam/model/dataLoaders/InventoryFromCSV.java
@@ -2,6 +2,8 @@ package model.dataLoaders;
 
 import model.store.Inventory;
 import model.store.Item;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 import java.io.IOException;
 
@@ -15,7 +17,23 @@ public class InventoryFromCSV
    public Inventory readFromCSV() throws IOException
    {
       Inventory result = new Inventory();
-      // TODO: add code that reads from CSV file and populates inventory
+      try
+      {
+         String row;
+         BufferedReader csvReader = new BufferedReader(new FileReader(fileName));
+         while ((row = csvReader.readLine()) != null)
+         {
+             String[] data = row.split(",");
+             int itemID = Integer.valueOf(data[0]);
+             float price = Float.valueOf(data[2]);
+             result.add(new Item(itemID, data[1], price));
+         }
+         csvReader.close();
+      }
+      catch (IOException e)
+      {
+         System.out.println("File not found");
+      }
       return result;
    }
 }
